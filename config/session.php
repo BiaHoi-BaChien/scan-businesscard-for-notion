49d2106d459b891a0706aebaf30cb76adcd1c34c
Pacho <93363437+BiaHoi-BaChien@users.noreply.github.com>
2025-11-28 16:55:40 +0700

    Set session cookies to SameSite=None by default

--- config/session.php
+++ config/session.php
@@ -18,7 +18,7 @@ return [
     ),
     'path' => '/',
     'domain' => env('SESSION_DOMAIN'),
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
     'http_only' => true,
-    'same_site' => env('SESSION_SAME_SITE', 'lax'),
+    'same_site' => env('SESSION_SAME_SITE', 'none'),
 ];
